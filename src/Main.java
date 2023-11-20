e595f6e62fa9c31d91ddb3a7e56348944ff81c23
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-11-20 11:54:21 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package src;
+public class Main {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int k = 1; k <= 5; k++) {
+            count++;
+        }
+        System.out.println(count);
+    }
+}
